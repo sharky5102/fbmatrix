68df31a9953de7381c4d1c2fc17d4d0e805b1660
Steve Hardy <steve@marjoleinensteve.nl>
2020-01-16 20:21:27 +0000

    Fix example bytearray

--- example_bytearray.py
+++ example_bytearray.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import fbmatrix
 import argparse
 import assembly.bytearray
@@ -16,11 +17,11 @@ parser = argparse.ArgumentParser(description='Amazing WS2811 VGA driver')
 parser.add_argument('--emulate', action='store_const', const=True, help='Emulate tree')
 parser.add_argument('--preview', action='store_const', const=True, help='Preview windows instead of actual output')
 parser.add_argument('--raw', action='store_const', const=True, help='Raw mode - use with --preview to view raw pixel data')
-parser.add_argument('--display', default='ws2811', help='Display type (ws2811, hub75e)')
+parser.add_argument('--display', default='hub75e', help='Display type (ws2811, hub75e)')
 
 args = parser.parse_args()
 
-matrix = fbmatrix.renderer(emulate=args.emulate, preview=args.preview, raw=args.raw, display=args.display)
+matrix = fbmatrix.renderer(emulate=args.emulate, preview=args.preview, raw=args.raw, display=args.display, supersample=0)
 
 bytearray = assembly.bytearray.bytearray()
 
